988d68d73e811d21bb6098a98e1d6fb876ef9908
jotdhessi <harmanjot28082004@gmail.com>
2023-12-05 13:49:03 -0500

    version3

--- Program1.py
+++ Program1.py
@@ -1,11 +1,11 @@
-x=int(input("Enter Number of years  "))
+a=int(input("Enter Number of years  "))
 total=0
 i=0
-z=x*12
-while(i<=z):
+c=x*12
+while(i<=c):
     y=float(input("The average high temperature  "))
     i=i+1
     total=total+y
 print("The Average High Temperature",total)  
-average=total/z
+average=total/c
 print("Temperture",average)
